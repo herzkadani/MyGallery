8e3cd2c5e6c31ee5fc3b8ec9a80b349d838cd36f
Dani Herzka <herzkadani@gmail.com>
2022-10-24 17:19:18 +0200

    no blob, activate auto increment

--- 151_projektarbeit.sql
+++ 151_projektarbeit.sql
@@ -23,15 +23,14 @@ DROP TABLE IF EXISTS `asset`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `asset` (
-  `id` int(11) NOT NULL,
-  `image` blob NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
   `fk_user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user_id` (`fk_user_id`),
   CONSTRAINT `asset_ibfk_1` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,7 +48,7 @@ CREATE TABLE `user` (
   `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -61,4 +60,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-21 15:52:52
+-- Dump completed on 2022-10-24 17:18:02
